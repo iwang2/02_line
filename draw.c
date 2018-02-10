553b62901a0919b28e96b289d856b08533eb3581
iwang2 <iwang2@stuy.edu>
2018-02-10 01:41:38 -0500

    was working before, but i broke it again :(

--- draw.c
+++ draw.c
@@ -14,19 +14,57 @@ void draw_line (int x0, int y0, int x1, int y1, screen s, color clr) {
     a = y1 - y0,
     b = x0 - x1,
     c = y0 * (x1 - x0);
+
+  int m;
+  if ( b == 0 ) m = 2;
+  else m = a / -b + 0.5;
   
   int d = a*x + b*y + c;
 
-  while ( x <= x1 ) {
+  while ( y <= y1 && (m >= 1 || m < -1) ) {
     plot(s, clr, x, y);
 
-    //if ( -b <= 1 && -b >= 0 ) {
+    // octant II
+    if ( a > 0 ) { 
+      if ( d < 0 ) {
+	x++;
+	d += 2*a;
+      }
+      y++;
+      d += 2*b;
+    }
+
+    // octant VII
+    else {
+      if ( d > 0 ) {
+	x++;
+	d += 2*a;
+      }
+      y--;
+      d += 2*b;
+    }
+  }
+
+  // I, VIII
+  while ( x <= x1 && (m < 1 || m >= -1) ) {
+    plot(s, clr, x, y);
+    
+    if ( a < 0 ){
+      if ( d < 0 ) {
+        y--;
+	d += 2*b;
+      }
+      x++;
+      d += 2*a;
+    }
+    
+    else {
       if ( d > 0 ) {
 	y++;
 	d += 2*b;
       }
       x++;
       d += 2*a;
-      //}
+    }
   }
 }
--- main.c
+++ main.c
@@ -17,10 +17,14 @@ int main() {
 
   clear_screen(s);
 
-  draw_line(0, 0, 300, 200, s, c); // oct I in green
+  draw_line(100, 100, 300, 500, s, c); // oct II in green
+  draw_line(100, 100, 500, 500, s, c); // m = 1
+  draw_line(100, 100, 500, 200, s, c); // oct I
 
   c.red = MAX_COLOR;
-  draw_line(100, 300, 400, 100, s, c); // oct VIII hopefully yellow
+  draw_line(100, 300, 400, 200, s, c); // oct VIII hopefully yellow
+  draw_line(100, 500, 400, 100, s, c); // oct VII
+  draw_line(100, 400, 400, 100, s, c); // m = -1
 
   c.green = 0;
   draw_line(0, 200, 500, 200, s, c); // horizontal in red
