361ffc986afd6769fb31c10b53ae0a52c31a5566
iwang2 <iwang2@stuy.edu>
2018-02-11 02:37:49 -0500

    still broken

--- draw.c
+++ draw.c
@@ -12,59 +12,66 @@ void draw_line (int x0, int y0, int x1, int y1, screen s, color clr) {
     y = y0;
   int
     a = y1 - y0,
-    b = x0 - x1,
-    c = y0 * (x1 - x0);
+    b = x0 - x1;
 
   int m;
   if ( b == 0 ) m = 2;
+  else if ( a < 0 ) m = a / -b - 0.5;
   else m = a / -b + 0.5;
+
+  int c = y - m*x;
   
   int d = a*x + b*y + c;
 
-  while ( y <= y1 && (m >= 1 || m < -1) ) {
-    plot(s, clr, x, y);
+  if( m >= 1 || m < -1 ) {
+    while ( y <= y1 ) {
+      plot(s, clr, x, y);
 
-    // octant II
-    if ( a > 0 ) { 
-      if ( d < 0 ) {
-	x++;
-	d += 2*a;
-      }
-      y++;
-      d += 2*b;
-    }
-
-    // octant VII
-    else {
-      if ( d > 0 ) {
-	x++;
-	d += 2*a;
-      }
-      y--;
-      d += 2*b;
-    }
-  }
-
-  // I, VIII
-  while ( x <= x1 && (m < 1 || m >= -1) ) {
-    plot(s, clr, x, y);
-    
-    if ( a < 0 ){
-      if ( d < 0 ) {
-        y--;
+      // VII
+      if ( a < 0 ) {
+	if ( d < 0 ) {
+	  x++;
+	  d += 2*a;
+	}
+	y--;
 	d += 2*b;
       }
-      x++;
-      d += 2*a;
-    }
-    
-    else {
-      if ( d > 0 ) {
+
+      // II
+      else { 
+	if ( d < 0 ) {
+	  x++;
+	  d += 2*a;
+	}
 	y++;
 	d += 2*b;
       }
-      x++;
-      d += 2*a;
+    }
+  }
+
+  else {
+    while ( x <= x1 ) {
+      plot(s, clr, x, y);
+
+      // VIII
+      if ( a < 0 ){
+	if ( d < 0 ) {
+	  y--;
+	  d += 2*b;
+	}
+	x++;
+	d += 2*a;
+      }
+
+      // I
+      else {
+	if ( d > 0 ) {
+	  y++;
+	  d += 2*b;
+	}
+	x++;
+	d += 2*a;
+      }
     }
   }
 }
