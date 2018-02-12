29dcd32818d4559c28ccd56a11a3555fb4d23766
iwang2 <iwang2@stuy.edu>
2018-02-11 22:11:44 -0500

    all octants workgit add .! just not vertical

--- draw.c
+++ draw.c
@@ -13,65 +13,66 @@ void draw_line (int x0, int y0, int x1, int y1, screen s, color clr) {
   int
     a = y1 - y0,
     b = x0 - x1;
+  int d;
 
-  int m;
+  double m;
   if ( b == 0 ) m = 2;
-  else if ( a < 0 ) m = a / -b - 0.5;
-  else m = a / -b + 0.5;
-
-  int c = y - m*x;
-  
-  int d = a*x + b*y + c;
-
-  if( m >= 1 || m < -1 ) {
-    while ( y <= y1 ) {
+  else m = a * 1.0 / -b;
+  printf("m = %f\n", m);
+    
+  // VII
+  if ( m < -1 ) {
+    d = a - 2*b;
+    while ( x <= x1 ) {
       plot(s, clr, x, y);
-
-      // VII
-      if ( a < 0 ) {
-	if ( d < 0 ) {
-	  x++;
-	  d += 2*a;
-	}
-	y--;
-	d += 2*b;
+      if ( d > 0 ) {
+	x++;
+	d += 2*a;
       }
-
-      // II
-      else { 
-	if ( d < 0 ) {
-	  x++;
-	  d += 2*a;
-	}
-	y++;
-	d += 2*b;
-      }
+      y--;
+      d -= 2*b;
     }
   }
 
-  else {
+  // VIII
+  else if ( m < 0 ) {
+    d = 2*a - b;
     while ( x <= x1 ) {
       plot(s, clr, x, y);
+      if ( d < 0 ) {
+	y--;
+	d -= 2*b;
+      }
+      x++;
+      d += 2*a;
+    }
+  }
 
-      // VIII
-      if ( a < 0 ){
-	if ( d < 0 ) {
-	  y--;
-	  d += 2*b;
-	}
+  // I
+  else if ( m < 1 ) {
+    d = 2*a + b;
+    while ( x <= x1 ) {
+      plot(s, clr, x, y);
+      if ( d > 0 ) {
+	y++;
+	d += 2*b;
+      }
+      x++;
+      d += 2*a;
+    }
+  }  
+
+  // II
+  else {
+    d = a + 2*b;
+    while ( x <= x1 ) {
+      plot(s, clr, x, y);
+      if ( d < 0 ) {
 	x++;
 	d += 2*a;
       }
-
-      // I
-      else {
-	if ( d > 0 ) {
-	  y++;
-	  d += 2*b;
-	}
-	x++;
-	d += 2*a;
-      }
+      y++;
+      d += 2*b;
     }
   }
 }
--- main.c
+++ main.c
@@ -17,21 +17,21 @@ int main() {
 
   clear_screen(s);
 
-  //draw_line(100, 100, 300, 500, s, c); // oct II in green
-  draw_line(100, 500, 400, 0, s, c); // oct VII
-  //draw_line(100, 100, 500, 500, s, c); // m = 1
+  draw_line(0, 0, 300, 500, s, c); // oct II in green
+  draw_line(0, 500, 300, 0, s, c); // oct VII
+  draw_line(0, 0, 500, 500, s, c); // m = 1
 
   c.red = MAX_COLOR;
-  draw_line(100, 300, 400, 200, s, c); // oct VIII hopefully yellow
-  //draw_line(100, 100, 500, 200, s, c); // oct I
-  //draw_line(100, 400, 400, 100, s, c); // m = -1
+  draw_line(0, 500, 500, 400, s, c); // oct VIII hopefully yellow
+  draw_line(0, 0, 500, 200, s, c); // oct I
+  draw_line(0, 500, 500, 0, s, c); // m = -1
 
   c.green = 0;
   draw_line(0, 200, 500, 200, s, c); // horizontal in red
 
   c.red = 0;
   c.blue = MAX_COLOR;
-  draw_line(300, 0, 300, 400, s, c); // vertical in blue
+  //draw_line(300, 0, 300, 400, s, c); // vertical in blue
 
   display(s);
   save_extension(s, "lines.png");
